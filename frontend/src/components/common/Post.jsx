1001baf2c1b3afcad60fb6277ed639ef28d19a52
Haroon Rasheed <hrq278@gmail.com>
2025-07-03 01:15:30 +0500

    Refactor Post component: move date formatting logic to a new utility file and clean up state variables

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -7,7 +7,10 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+
 import LoadingSpinner from "./LoadingSpinner";
+
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 
@@ -15,7 +18,14 @@ const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
 
 	const {data:authUser } = useQuery({queryKey: ["authUser"]	})
+	
 	const queryClient = useQueryClient()
+
+	const postOwner = post.user;
+	const isLiked = post.likes.includes(authUser._id)
+	const isMyPost = authUser._id === post.user._id;
+	const formattedDate = formatPostDate(post.createdAt) ;
+
 
 	const {mutate:deletePost, isPending: isDeleting} = useMutation({
 		mutationFn: async () => {
@@ -105,12 +115,6 @@ const Post = ({ post }) => {
 		}
 	})
 
-	const postOwner = post.user;
-	const isLiked = post.likes.includes(authUser._id)
-
-	const isMyPost = authUser._id === post.user._id;
-
-	const formattedDate = "1h";
 
 	const handleDeletePost = () => {
 		deletePost()
